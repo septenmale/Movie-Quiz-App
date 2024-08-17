8d88c6dc9ea64fb0508f3ade6f49e6440be73294
Septenmale <zavhorodnii.viktor@icloud.com>
2024-08-17 19:39:48 +0200

    Alert presentor classs has been implemented; no errors

---
+++ MovieQuiz/Models/AlertModel.swift
@@ -0,0 +1,8 @@
+//
+//  AlertModel.swift
+//  MovieQuiz
+//
+//  Created by Viktor on 17/08/2024.
+//
+
+import Foundation
---
+++ MovieQuiz/Presentation/AlertPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  AlertPresenter.swift
+//  MovieQuiz
+//
+//  Created by Viktor on 17/08/2024.
+//
+
+import Foundation
